c0a81ae9c2883aa3f9d47ecf291dd229ea9d211e
Nijenhuis <Nico.Nijenhuis@vgz.nl>
2019-04-08 10:59:35 +0000

    Toegevoegde functionaliteit

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print 'hello world'
+'''
+Als het goed is tellen we nu
+twee regels met commentaar
+'''
+
+import os
+import sys
+lijst=[]
+item={}
+item['Name']='John'
+lijst.append(item)
+item={}
+item['Name']='Klaas'
+lijst.append(item)
+for item in  lijst:
+	print item['Name']
+
+
+
